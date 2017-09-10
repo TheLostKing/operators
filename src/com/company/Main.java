a83996070b34b8f6971acf11d2e813481b5bd7f3
TheLostKing <calderonezj@gmail.com>
2017-09-09 21:40:35 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("6 * 9 = " + (6*9));
+        System.out.println("3 = 5, " + (3==5));
+        System.out.println("3/4 = " + (3/4));
+        System.out.println("10 is odd, " + ((10%2)!=0));
+    }
+}
